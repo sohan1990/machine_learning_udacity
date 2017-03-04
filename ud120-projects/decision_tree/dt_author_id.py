ebe412e22a61fbc202175232bce43805ff9afa7b
Sohan Samanta <sohansamanta@r02ssqt3.device.mst.edu>
2017-03-03 22:03:59 -0600

    Decision Tree mini project completed

--- ud120-projects/decision_tree/dt_author_id.py
+++ ud120-projects/decision_tree/dt_author_id.py
@@ -12,7 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -20,12 +21,20 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
 
+# find no of features
+print(features_train.shape)
+print(len(features_train[0]))
 
+# defining classifier and training program
+clf = DecisionTreeClassifier(min_samples_split = 40)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+acc = accuracy_score(pred, labels_test)
+print(acc)
 #########################################################
 
 
