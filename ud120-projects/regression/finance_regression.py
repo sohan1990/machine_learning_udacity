14cc9dc2a071b37d8d086065a0d03c8e36a03552
Sohan Samanta <sohansamanta@r02ssqt3.device.mst.edu>
2017-03-19 20:37:44 -0500

    Regression updated

--- ud120-projects/regression/finance_regression.py
+++ ud120-projects/regression/finance_regression.py
@@ -10,8 +10,7 @@
     Draws a little scatterplot of the training/testing data
 
     You fill in the regression code where indicated:
-"""    
-
+"""
 
 import sys
 import pickle
@@ -22,28 +21,31 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
+#features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+#sort_keys = '../tools/python2_lesson06_keys.pkl'       # dictionary key order, for python 3.3 (in 3.3 -> it is randomized)
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
-
-
+test_color = "r"
 
 ### Your regression goes here!
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
 
-
-
-
-
-
+print "slope =", reg.coef_
+print reg.intercept_
+print reg.score(feature_train,target_train)
+#pred = reg.predict(feature_test)
+print reg.score(feature_test, target_test)
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
@@ -56,14 +58,14 @@ for feature, target in zip(feature_train, target_train):
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
 plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
-
-
-
 ### draw the regression line, once it's coded
 try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+print "new slope =", reg.coef_
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
