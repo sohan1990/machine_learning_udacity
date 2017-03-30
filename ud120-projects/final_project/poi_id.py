bfeaf0377c2f069a62792590f8843cffacf2199c
Sohan Samanta <sohansamanta@Sohans-MacBook-Pro.local>
2017-03-29 23:29:04 -0500

    Classifiers added

--- ud120-projects/final_project/poi_id.py
+++ ud120-projects/final_project/poi_id.py
@@ -11,15 +11,85 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 
+### classifier function
+def classifier_list():
+
+    clf_list = []
+    # NAIVE BAYES
+    from sklearn.naive_bayes import GaussianNB
+    clf_NB = GaussianNB()
+    clf_NB_params = {}
+    clf_list.append((clf_NB, clf_NB_params))
+
+    # SVM
+    from sklearn.svm import LinearSVC
+    clf_svm = LinearSVC()
+    clf_svm_params = {"C": [1, 5, 10, 15, 20],
+                     "tol": [0.1, 0.01, 0.001, 0.0001],
+                     "dual": False}
+    clf_list.append((clf_svm, clf_svm_params))
+
+    # DECISION TREE
+    from sklearn.tree import DecisionTreeClassifier
+    clf_tree = DecisionTreeClassifier()
+    clf_tree_params = {"min_samples_split": [2,5,10,15,20],
+                      "criterion": ["gini", "entropy"]}
+    clf_list.append((clf_tree, clf_tree_params))
+
+    # ADABOOST
+    from sklearn.ensemble import AdaBoostClassifier
+    clf_ada = AdaBoostClassifier()
+    clf_ada_params = {"n_estimators": [20, 30, 40, 50],
+                      "learning_rate": [0.1, 0.5, 1, 2, 5],
+                      "algorithm": ["SAMME", "SAMME.R"]}
+    clf_list.append((clf_ada, clf_ada_params))
+
+    # RANDOM FOREST
+    from sklearn.ensemble import RandomForestClassifier
+    clf_rforest = RandomForestClassifier()
+    clf_rforest_params = {"n_estimators": [20, 30, 40, 50],
+                          "criterion": ["gini", "entropy"],
+                          "min_samples_split": [2, 5, 10, 15, 20]}
+    clf_list.append((clf_rforest, clf_rforest_params))
+
+    # LOGISTIC REGRESSION
+    from sklearn.linear_model import LogisticRegression
+    clf_logreg = LogisticRegression()
+    clf_logreg_params = {"C": [0.1, 1, 10, 50, 100],
+                         "tol": [0.1, 0.01, 0.001, 0.0001],}
+    clf_list.append((clf_logreg, clf_logreg_params))
+
+    return clf_list
+
+
+
+
+
+
+
+
+
+
 
 
 ### Task 1: Select what features you'll use.
 
 poi = ['poi']
 
-financial_features = ['salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus',
-                     'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses',
-                     'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 'director_fees']
+financial_features = ['salary',
+                      'deferral_payments',
+                      'total_payments',
+                      'loan_advances',
+                      'bonus',
+                      'restricted_stock_deferred',
+                      'deferred_income',
+                      'total_stock_value',
+                      'expenses',
+                      'exercised_stock_options',
+                      'other',
+                      'long_term_incentive',
+                      'restricted_stock',
+                      'director_fees']
 
 #email_features = ['to_messages', 'email_address', 'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi',
 #                 'shared_receipt_with_poi']
